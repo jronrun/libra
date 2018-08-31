5e2a731cf43f0766da3b030f6f494324eea2c95f
yejiwei <yejiwei@u51.com>
2018-08-31 11:23:55 +0800

    mirror themes

--- resources/utils/CMAssist.js
+++ resources/utils/CMAssist.js
@@ -10,15 +10,14 @@ const thirdThemes = [
   'tomorrow-night-blue', 'toy-chest'
 ]
 const themes = [...[
-  'default', 'lemon', '3024-day', '3024-night', 'abcdef', 'ambiance', 'base16-dark', 'base16-light', 'bespin',
-  'blackboard', 'cobalt',
-  'colorforth', 'dracula', 'eclipse', 'elegant', 'erlang-dark', 'hopscotch', 'icecoder', 'isotope', 'lesser-dark',
-  'liquibyte',
-  'material', 'mbo', 'mdn-like', 'midnight', 'monokai', 'neat', 'neo', 'night', 'paraiso-dark', 'paraiso-light',
-  'pastel-on-dark',
-  'railscasts', 'rubyblue', 'seti', 'solarized,dark', 'solarized,light', 'the-matrix', 'tomorrow-night-bright',
-  'tomorrow-night-eighties',
-  'ttcn', 'twilight', 'vibrant-ink', 'xq-dark', 'xq-light', 'yeti', 'zenburn'
+  'default', 'lemon', '3024-day', '3024-night', 'abcdef', 'ambiance', 'ambiance-mobile', 'base16-dark', 'base16-light',
+  'bespin', 'blackboard', 'cobalt', 'colorforth', 'darcula', 'dracula', 'duotone-dark', 'duotone-light', 'eclipse',
+  'elegant', 'erlang-dark', 'gruvbox-dark', 'hopscotch', 'icecoder', 'idea', 'isotope', 'lesser-dark', 'liquibyte',
+  'lucario', 'material', 'mbo', 'mdn-like', 'midnight', 'monokai', 'neat', 'neo', 'night', 'oceanic-next',
+  'panda-syntax',
+  'paraiso-dark', 'paraiso-light', 'pastel-on-dark', 'railscasts', 'rubyblue', 'seti', 'shadowfox', 'solarized', 'ssms',
+  'the-matrix', 'tomorrow-night-bright', 'tomorrow-night-eighties', 'ttcn', 'twilight', 'vibrant-ink', 'xq-dark',
+  'xq-light', 'yeti', 'zenburn'
 ], ...thirdThemes]
 
 const blackBGMark = [
