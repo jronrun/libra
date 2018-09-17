f927c1d4804a2b4518dd82b01dac03fd2df96c7b
yejiwei <yejiwei@u51.com>
2018-09-17 15:49:05 +0800

    highlights.js

---
+++ resources/plugins/highlights.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import CodeMirror from 'codemirror/lib/codemirror'
+import 'codemirror/lib/codemirror.css'
+
+import 'codemirror/addon/runmode/runmode'
+
+import CMAssist, { highlights } from '../assets/CMAssistConfig'
+
+export { CMAssist, highlights }
