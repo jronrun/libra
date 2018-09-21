1426e8b454d3ed431df1cb853b034e278021295e
yejiwei <yejiwei@u51.com>
2018-09-21 14:53:55 +0800

    anchor link symbol

--- resources/assets/MarkdownAssist.js
+++ resources/assets/MarkdownAssist.js
@@ -18,6 +18,7 @@ import pluginIns from 'markdown-it-ins'
 import 'github-markdown-css'
 
 const features = Symbol('features')
+const anchorLinkSymbol = '<svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M4 9h1v1H4c-1.5 0-3-1.69-3-3.5S2.55 3 4 3h4c1.45 0 3 1.69 3 3.5 0 1.41-.91 2.72-2 3.25V8.59c.58-.45 1-1.27 1-2.09C10 5.22 8.98 4 8 4H4c-.98 0-2 1.22-2 2.5S3 9 4 9zm9-3h-1v1h1c1 0 2 1.22 2 2.5S13.98 12 13 12H9c-.98 0-2-1.22-2-2.5 0-.83.42-1.64 1-2.09V6.25c-1.09.53-2 1.84-2 3.25C6 11.31 7.55 13 9 13h4c1.45 0 3-1.69 3-3.5S14.5 6 13 6z"></path></svg>'
 
 // https://markdown-it.github.io/
 // Inject line numbers for sync scroll. Notes:
@@ -70,7 +71,7 @@ class MarkdownAssist {
     // Highlight options, see CMAssist.getHighlight
     CMAssistHighlightOptions = {},
     // Render
-    template = (result) => `<article class="markdown-body" style="padding: 1rem;">${result}</article>`,
+    template = (result) => `<article class="markdown-body" style="padding: 2.3rem;">${result}</article>`,
     style = '.warning { background-color: #eaea83; padding: 12px; border-radius: 6px;}'
   }) {
     this.isCMAssistHighlight = null == highlight && null != CMAssist
@@ -112,9 +113,9 @@ class MarkdownAssist {
       .use(pluginContainer, 'warning')
       .use(pluginIns)
 
-    this.instance.renderer.rules.paragraph_open =
-      this.instance.renderer.rules.heading_open =
-        injectLineNumbers
+    // this.instance.renderer.rules.paragraph_open =
+    //   this.instance.renderer.rules.heading_open =
+    //     injectLineNumbers
 
     this.CMAssist = CMAssist
     this.CMAssistHighlightOptions = CMAssistHighlightOptions
@@ -136,6 +137,11 @@ class MarkdownAssist {
       div.setAttribute('style', 'display:none;')
       div.innerHTML = result
       pi.query('body').appendChild(div)
+
+      const anchors = pi.querySelector(`#${hlId} a.anchor`, true)
+      for (let elem of anchors.values()) {
+        elem.innerHTML = anchorLinkSymbol
+      }
 
       const hlEls = pi.querySelector(`#${hlId} pre.mirror-hl`, true)
       for (let elem of hlEls.values()) {
